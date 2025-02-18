b3ec5b9aff4c2d57dc1a3e00ae9cc0570b099389
Janus Code <januscodee@gmail.com>
2025-02-18 01:54:50 +0000

    Update storage-mock.ts

--- test-config/storage-mock.ts
+++ test-config/storage-mock.ts
@@ -1,4 +1,4 @@
-import { MainProtocolSymbols } from 'airgap-coin-lib/dist/utils/ProtocolSymbols'
+import { MainProtocolSymbols } from 'januscode/dist/utils/ProtocolSymbols'
 
 export class StorageMock {
   private readonly data: any = {
